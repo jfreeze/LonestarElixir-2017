4a4a529aa63758d34eafa2d91f4555b644e1858c
Jim Freeze <jimfreeze@jimsdevmbp.attlocal.net>
2016-10-20 12:21:14 -0500

    updated release script to stop/start

--- update-release.sh
+++ update-release.sh
@@ -8,5 +8,5 @@ node node_modules/brunch/bin/brunch build --production
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix phoenix.digest
 MIX_ENV=prod mix release
-rel/lonestarelixir/bin/lonestarelixir restart
-
+rel/lonestarelixir/bin/lonestarelixir stop
+rel/lonestarelixir/bin/lonestarelixir start
