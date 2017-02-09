2b2017bf9eae295abfd0030fff7a9ab6ca570d77
Luke Imhoff <limhoff@csd.org>
2017-02-08 20:59:15 -0600

    Add iCal ics file links for schedule, days, and sessions
    ics files are supported by macOS iCal (where they originated) and also
    Outlook, but Google Calendar doesn't associate with them, you need to
    import them manually.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule Lonestarelixir.PageView do
   use Lonestarelixir.Web, :view
+
+  def ics_link(path, opts \\ []) do
+    link(to: "#{path}.ics", title: Keyword.get(opts, :title, "Add session to iCal/Outlook")) do
+      tag(:img,  height: "32px", src: "/images/ical.png", width: "32px")
+    end
+  end
 end
