339b57b3ad813051ddc01092d5203ba6e936f922
Jim Freeze <jimfreeze@jimsdevmbp.attlocal.net>
2017-02-09 09:57:39 -0600

    updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Lonestarelixir.Mixfile do
 
   def project do
     [app: :lonestarelixir,
-     version: "0.0.15",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
