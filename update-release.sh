59df0a9d82434145399667a33c7441599c5c24fc
Jim Freeze <jimfreeze@jimsdevmbp.attlocal.net>
2017-03-02 15:55:00 -0600

    new phx.digest

--- update-release.sh
+++ update-release.sh
@@ -6,7 +6,7 @@ git pull
 mix do deps.get
 node node_modules/brunch/bin/brunch build --production
 MIX_ENV=prod mix compile
-MIX_ENV=prod mix phoenix.digest
+MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
 rel/lonestarelixir/bin/lonestarelixir stop
 rel/lonestarelixir/bin/lonestarelixir start
