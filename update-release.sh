863cde0f140ba395bea8bb2cbc3f4094469a7c68
Jim Freeze <jimfreeze@jimsdevmbp.attlocal.net>
2017-03-02 15:57:06 -0600

    new brunch-config.js path

--- update-release.sh
+++ update-release.sh
@@ -4,7 +4,7 @@
 
 git pull
 mix do deps.get
-node node_modules/brunch/bin/brunch build --production
+node assets/node_modules/brunch/bin/brunch build --production
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
